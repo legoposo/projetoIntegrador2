871a84976a2d758442e80273b9d7dca728ecf135
Leonardo de Souza <legoposo@gmail.com>
2025-10-20 11:06:42 -0300

    Corrige CSS para o Render

--- core/settings.py
+++ core/settings.py
@@ -1,25 +1,29 @@
 import os
 from pathlib import Path
 
+# Caminho base do projeto
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Segurança
 SECRET_KEY = 'django-insecure-%=5r)k$(m&4sz#ycvf9*wvb_6q%hpbkg(bczdlq)gy-5j5sz4m'
 DEBUG = False
 ALLOWED_HOSTS = [
-    'projetointegrador2-nvi0.onrender.com',  # URL real do Render
+    'projetointegrador2-nvi0.onrender.com',  # URL do Render
     'localhost',
     '127.0.0.1',
 ]
 
+# Para HTTPS no Render
 CSRF_TRUSTED_ORIGINS = [
-    'https://projetointegrador2-nvi0.onrender.com',  # necessário com HTTPS
+    'https://projetointegrador2-nvi0.onrender.com',
 ]
 
-STATIC_URL = 'static/'
+# Arquivos estáticos
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-
+# Aplicativos instalados
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -35,9 +39,10 @@ INSTALLED_APPS = [
     'medicos',
 ]
 
+# Middleware
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # Serve arquivos estáticos
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -46,9 +51,11 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+# URLs e WSGI
 ROOT_URLCONF = 'core.urls'
 WSGI_APPLICATION = 'core.wsgi.application'
 
+# Banco de dados
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -56,6 +63,7 @@ DATABASES = {
     }
 }
 
+# Validação de senhas
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
     {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
@@ -63,13 +71,16 @@ AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
 
+# Internacionalização
 LANGUAGE_CODE = 'pt-br'
 TIME_ZONE = 'America/Sao_Paulo'
 USE_I18N = True
 USE_TZ = True
 
+# Modelos padrão
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# Templates
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -86,9 +97,12 @@ TEMPLATES = [
     },
 ]
 
-LOGIN = 'conta/entrar'
+# Login
+LOGIN_URL = 'conta/entrar'
 LOGIN_REDIRECT_URL = '/'
-LOGOUT = 'logout'
+LOGOUT_REDIRECT_URL = 'logout'
+
+# Custom User
 AUTH_USER_MODEL = 'accounts.User'
 AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
